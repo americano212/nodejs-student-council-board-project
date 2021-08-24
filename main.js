89520af5a83242e4c6339d47c3c3684441ec567b
DongJun Shin <wq0212@naver.com>
2021-08-24 21:10:53 +0900

    add url in main.js

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-        url = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
+    var _url = request.url;
+    if (_url == '/favicon.ico'){
         return response.writeHead(404);
     }
+    if(_url == '/'){
+        _url = '/index.html';
+    }
+    else if (_url == '/login') {
+        _url = '/login.html';
+    }
+    else if (_url == '/register') {
+        _url = '/register.html';
+    }
+    else if (_url == '/board') {
+        _url = '/board.html';
+    }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(fs.readFileSync(__dirname + _url));
 });
 
 app.listen(3000);
