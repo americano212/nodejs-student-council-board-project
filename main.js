0f198ef88fc1763ae24fa198514783b61308896f
DongJun Shin <wq0212@naver.com>
2021-08-26 20:22:17 +0900

    change dir and make board.html

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var mysql = require('mysql');
+var template = require('./template.js');
+
+var db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'qw0212',
+    database : 'test'
+});
+db.connect();
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
-    if (_url == '/favicon.ico'){
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if (pathname == '/favicon.ico'){
         return response.writeHead(404);
     }
-    if(_url == '/'){
+    if(pathname == '/'){
         _url = '/index.html';
     }
-    else if (_url == '/login') {
+    else if (pathname == '/login') {
         _url = '/index.html';
     }
-    else if (_url == '/register') {
+    else if (pathname == '/register') {
         _url = '/register.html';
     }
-    else if (_url == '/board') {
+    else if (pathname == '/board') {
+        _url = '/board.html';
+        db.query(`SELECT * FROM tblBoard`,function(error, contents){
+            var title = '';
+            var list = template.list(contents);
+            var html = template.HTML(title,list,``,``);
+        });
+    }
+    else if (pathname == '/notice') {
         _url = '/index.html';
     }
-    else if (_url == '/notice') {
+    else if (pathname == '/write') {
         _url = '/index.html';
     }
-    else if (_url == '/write') {
-        _url = '/index.html';
-    }
-    else if (_url == '/mypage') {
+    else if (pathname == '/mypage') {
         _url = '/index.html';
     }
     response.writeHead(200);
