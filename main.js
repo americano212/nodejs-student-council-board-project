4e2d882b23ecc1b44bda274fc9f9dda4eec7cfaa
DongJun Shin <wq0212@naver.com>
2021-09-19 14:36:26 +0900

    fix comment err

--- main.js
+++ main.js
@@ -340,12 +340,13 @@ app.post('/detail/:id', (req,res) => {
         if (err){
           throw err;
         }
-        if (result[0].u_status == 7){
-            r_s = 1;
-        }
+
         const sql = 'INSERT INTO tblreply (r_content_seq,r_writer_seq,r_content,r_created,r_status) VALUES';
         const sqlValue = `("${id}","${req.session.u_seq}","${reply}",NOW(),"${r_s}");`;
         if(auth){
+            if (result[0].u_status == 7){
+                r_s = 1;
+            }
             if(!reply){
               res.send(`<script>alert('내용을 입력해주세요');location.href='/detail/${id}';</script>`);
             }
